eb82764c0192a6a678d9053cd0975ff337c5ac95
omarabousena <omarali97@gmail.com>
2020-05-30 10:32:38 +0200

    XHR test

--- scripts.js
+++ scripts.js
@@ -271,7 +271,22 @@ var url = 'http://getfromtable.azurewebsites.net/api/getfromtable';
 var xhr = createCORSRequest('GET', url);
 xhr.send();
 
+var data = "{url=\"https://storageraumueberwachung.table.core.windows.net/Last2()?sv=2019-10-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2023-07-31T22:47:36Z&st=2020-05-13T14:47:36Z&spr=https&sig=4skbsTU9tMlfSqFJhB1rTw16nVRIA6EVzxEkCmrNneE%3D\"}";
 
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("GET", "http://getfromtable.azurewebsites.net/api/getfromtable");
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "text/plain");
+
+xhr.send(data);
 
 
 
